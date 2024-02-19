623974fe0f3e026571c1d5f04f38425219f04266
RonJurincie <ronjurincie@gmail.com>
2024-02-19 13:16:28 -0500

    READY for TESTING and CreateNewChordView

--- iJamGuitar/Views/StringsView.swift
+++ iJamGuitar/Views/StringsView.swift
@@ -32,91 +32,153 @@ import OSLog
 struct StringsView: View {
     @Query var appStates: [AppState]
     @State private var dragLocation: CGPoint?
-    let halfStringWidth = 10.0
     let audioManager = iJamAudioManager()
     var height: CGFloat
+    let kNoFret = -1
+    @State private var formerZone = -1
+    @State private var zoneBreaks:[Double] = Array(repeating: 0.0, count: 6)
     var drag: some Gesture {
         DragGesture()
-            .onEnded { _ in audioManager.formerZone = -1 }
+            .onEnded { _ in formerZone = -1 }
             .onChanged { drag in
-            dragLocation = drag.location
-                audioManager.newDragLocation(dragLocation)
-                Logger.viewCycle.debug("Drag[x] = \(drag.location.x)")
-        }
+                dragLocation = drag.location
+                newDragLocation(dragLocation)
+            }
     }
     
     init(height: CGFloat) {
         self.height = height
     }
-
+    
     var body: some View {
         let appState = appStates.first!
-        HStack() {
-            SixSpacerHStack()
+        HStack(spacing:0) {
+            SixHorizontalSpacers()
+            SixHorizontalSpacers()
             
-            HStack() {
-                SixSpacerHStack()
-                HStack(spacing:0) {
-                    ForEach(0...5, id: \.self) { index in
-                        StringView(height: height,
-                                   stringNumber: 6 - index)
-                        .readFrame { newFrame in
-                            audioManager.zoneBreaks[index] = (newFrame.maxX + newFrame.minX) / 2.0
-                        }
-                    }
-                    SixSpacerHStack()
-                }
-                .task({ await playOpeningArpegio() })
-                .contentShape(Rectangle())
-                .gesture(drag)
-                .alert("Master Volume is OFF",
-                       isPresented: Bindable(appState).showVolumeAlert) {
-                    Button("OK", role: .cancel) { appState.showVolumeAlert = false }
-                }
-                .alert("Another App is using the Audio Player",
-                       isPresented: Bindable(appState).showAudioPlayerInUseAlert) {
-                    Button("OK", role: .cancel) { appState.showAudioPlayerInUseAlert = false }
-                }
-                       .alert("Unknown Audio Player Error", isPresented: Bindable(appState).showAudioPlayerErrorAlert) {
-                    Button("OK", role: .cancel) { fatalError() }
+            ForEach(0...5, id: \.self) { index in
+                StringView(height: height,
+                           stringNumber: 6 - index)
+                .readFrame { newFrame in
+                    zoneBreaks[index] = (newFrame.maxX + newFrame.minX) / 2.0
                 }
             }
-            SixSpacerHStack()
+            
+            SixHorizontalSpacers()
+            SixHorizontalSpacers()
         }
-        .task({await playOpeningArpegio()})
+        .task({ await playOpeningArpegio() })
         .contentShape(Rectangle())
         .gesture(drag)
-        .alert("Master Volume is OFF", isPresented: Bindable(appStates.first!).showVolumeAlert) {
-            Button("OK", role: .cancel) { appStates.first!.showVolumeAlert = false }
+        .alert("Master Volume is OFF",
+               isPresented: Bindable(appState).showVolumeAlert) {
+            Button("OK", role: .cancel) { appState.showVolumeAlert = false }
         }
-        .alert("Another App is using the Audio Player", isPresented: Bindable(appStates.first!).showAudioPlayerInUseAlert) {
-            Button("OK", role: .cancel) { appStates.first!.showAudioPlayerInUseAlert = false }
+        .alert("Another App is using the Audio Player",
+              isPresented: Bindable(appState).showAudioPlayerInUseAlert) {
+           Button("OK", role: .cancel) { appState.showAudioPlayerInUseAlert = false }
+               }
+        .alert("Unknown Audio Player Error", isPresented: Bindable(appState).showAudioPlayerErrorAlert) {
+          Button("OK", role: .cancel) { fatalError() }
         }
-        .alert("Unknown Audio Player Error", isPresented: Bindable(appStates.first!).showAudioPlayerErrorAlert) {
-            Button("OK", role: .cancel) { fatalError() }
+    }
+    
+    struct SixHorizontalSpacers: View {
+        var body: some View {
+            HStack() {
+                ForEach(0...5, id: \.self) { _ in
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+extension StringsView {
+    // Drag Management
+    func getZone(loc: CGPoint) -> Int{
+        // ZoneBreaks[n] is left-most position of string[n + 1]
+        var zone = 0
+        
+        if loc.x < zoneBreaks[0] {
+            zone = 6    // left of string 6
+        } else if loc.x < zoneBreaks[1] {
+            zone = 5    // between string 6 and string 5
+        } else if loc.x < zoneBreaks[2] {
+            zone = 4  // between string 5 and string 4
+        } else if loc.x < zoneBreaks[3] {
+            zone = 3  // between string 4 and string 3
+        } else if loc.x < zoneBreaks[4] {
+            zone = 2  // between string 3 and string 2
+        } else if loc.x < zoneBreaks[5] {
+            zone = 1  // between string 2 and string 1
+        } else {
+            zone = 0  // right of string 1
+        }
+        
+        return zone
+    }
+    
+    func stringNumberToPlay(zone: Int, oldZone: Int) -> Int {
+        return oldZone > zone ? oldZone : zone
+    }
+    
+    ///   Description: This method determines if we are in a new zone -
+    ///   and if we should then play note on appropriate string
+    /// - Parameter location:- the current location of the drag in global co-ordinates
+    func newDragLocation(_ location: CGPoint?) {
+        guard let location =  location else { return }
+        Logger.viewCycle.debug("Drag[x] = \(location.x)")
+        let zone = getZone(loc: location)
+        if zone != formerZone {
+            Logger.viewCycle.notice("====> In New Zone: \(zone)")
+            
+            if formerZone >= 0 && appStates.first!.isMuted == false {
+                //                 if audioManager.AVAudioSession.sharedInstance().outputVolume == 0.0 {
+                //                     appStates.first!.showVolumeAlert = true
+                //                 } else {
+                
+                let stringToPlay: Int = stringNumberToPlay(zone: zone, oldZone: formerZone)
+                pickString(stringToPlay)
+                //                 }
+            }
+            
+            formerZone = zone
+        }
+    }
+    
+    /// Description: This method identifies the note to play on this string based on capo position and fret -
+    ///  and then plays that string if the string is not muted
+    /// - Parameter stringToPlay: The String to be played
+    func pickString(_ stringToPlay: Int) {
+        let openNotes = appStates.first!.activeTuning?.openNoteIndices.components(separatedBy: "-")
+        
+        let fretPosition = appStates.first!.currentFretIndexMap[6 - stringToPlay]
+        if fretPosition > kNoFret {
+            if let noteIndices = openNotes, let thisStringsOpenIndex = Int(noteIndices[6 - stringToPlay]) {
+                let index = fretPosition + thisStringsOpenIndex + appStates.first!.capoPosition
+                let noteToPlayName = audioManager.noteNamesArray[index]
+                
+                let volume = appStates.first!.volumeLevel
+                Logger.viewCycle.debug("playing string: \(stringToPlay)")
+                do {
+                    try audioManager.playWaveFile(noteName:noteToPlayName,
+                                                  stringNumber: stringToPlay,
+                                                  volume: volume / 5.0)
+                } catch {
+                    Logger.viewCycle.error("Could not play wave file...")
+                }
+            }
         }
     }
     
     func playOpeningArpegio() async {
-        Logger.viewCycle.debug("ZoneBreaks: \(audioManager.zoneBreaks)")
         for string in 0...5 {
-            audioManager.pickString(6 - string)
+            pickString(6 - string)
             try? await Task.sleep(nanoseconds: 0_100_000_000)
         }
-        
-        Logger.viewCycle.debug("zoneBreaks: \(audioManager.zoneBreaks)")
+        Logger.viewCycle.debug("-----> zoneBreaks: \(zoneBreaks)")
     }
 }
-    
-struct SixSpacerHStack: View {
-    var body: some View {
-        HStack() {
-            ForEach(0...5, id: \.self) { _ in
-                Spacer()
-            }
-        }
-    }
-}
-
 
 
--- iJamGuitar/iJamAudioManger.swift
+++ iJamGuitar/iJamAudioManger.swift
@@ -14,6 +14,8 @@ enum InitializeErrors: Error {
     case InitializeSoundsError
     case MissingResourseError
     case AVAudioSessionError
+    case AVAudioPlayerInUse
+    case UnknownError
 }
 
 struct FramePreferenceKey: PreferenceKey {
@@ -22,132 +24,51 @@ struct FramePreferenceKey: PreferenceKey {
 }
 
 class iJamAudioManager {
-    @Environment(\.modelContext) var modelContext
-    var appState: AppState?
-    let kNoFret             = -1
-    let kHalfStringWidth    = 5.0
-    var formerZone          = -1
-    var zoneBreaks:[Double] = Array(repeating: 0.0, count: 6)
     var audioPlayerArray    = [AVAudioPlayer?]() // 1 audioPlayer for each string 6-1
     var noteNamesArray      = ["DoubleLow_C.wav", "DoubleLow_C#.wav", "DoubleLow_D.wav", "DoubleLow_D#.wav", "Low_E.wav", "Low_F.wav", "Low_F#.wav", "Low_G.wav", "Low_G#.wav", "Low_A.wav", "Low_A#.wav", "Low_B.wav", "Low_C.wav", "Low_C#.wav", "Low_D.wav", "Low_D#.wav", "E.wav", "F.wav", "F#.wav", "G.wav", "G#.wav", "A.wav", "A#.wav", "B.wav", "C.wav", "C#.wav", "D.wav", "D#.wav", "High_E.wav", "High_F.wav", "High_F#.wav", "High_G.wav", "High_G#.wav", "High_A.wav", "High_A#.wav", "High_B.wav", "High_C.wav", "High_C#.wav", "High_D.wav", "High_D#.wav", "DoubleHigh_E.wav", "DoubleHigh_F.wav", "DoubleHigh_F#.wav"]
     
-    init(formerZone: Int = -1) {
-        self.formerZone = formerZone
-        
-        initializeAVAudioSession()
-        loadWaveFilesIntoAudioPlayers()
+    init() {
+        try? initializeAVAudioSession()
+        try? loadWaveFilesIntoAudioPlayers()
     }
    
-    func initializeAVAudioSession() {
+    func initializeAVAudioSession() throws {
         do {
             // Attempts to activate session so you can play audio,
             // if other sessions have priority this will fail
             try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
             try AVAudioSession.sharedInstance().setActive(true)
         } catch let error {
-            appState?.showAudioPlayerErrorAlert = true
             Logger.statistics.error("\(error.localizedDescription)")
-            fatalError()
+            throw InitializeErrors.AVAudioSessionError
         }
     }
     
     /// This method instantiates one AudioPlayer for each string
-    func loadWaveFilesIntoAudioPlayers() {
+    func loadWaveFilesIntoAudioPlayers() throws {
         for _ in 0...5 {
             if let asset = NSDataAsset(name:"NoNote") {
                 do {
                     let thisAudioPlayer = try AVAudioPlayer(data:asset.data,
                                                             fileTypeHint:"wav")
                     if thisAudioPlayer.isPlaying {
-                        appState?.showAudioPlayerInUseAlert = true
+                        throw InitializeErrors.AVAudioPlayerInUse
                     }
                     audioPlayerArray.append(thisAudioPlayer)
                 } catch InitializeErrors.AVAudioSessionError{
                     fatalError()
                 }
                 catch {
-                    appState?.showAudioPlayerErrorAlert = true
                     Logger.errors.error("\(error.localizedDescription)")
-                    fatalError()
+                    throw InitializeErrors.InitializeSoundsError
                 }
             }
         }
     }
-    
-    func getZone(loc: CGPoint) -> Int{
-        // ZoneBreaks[n] is left-most position of string[n + 1]
-        var zone = 0
-        
-        if loc.x < zoneBreaks[0] {
-            zone = 6    // left of string 6
-        } else if loc.x < zoneBreaks[1] {
-            zone = 5    // between string 6 and string 5
-        } else if loc.x < zoneBreaks[2] {
-            zone = 4  // between string 5 and string 4
-        } else if loc.x < zoneBreaks[3] {
-            zone = 3  // between string 4 and string 3
-        } else if loc.x < zoneBreaks[4] {
-            zone = 2  // between string 3 and string 2
-        } else if loc.x < zoneBreaks[5] {
-            zone = 1  // between string 2 and string 1
-        } else {
-            zone = 0  // right of string 1
-        }
-        
-        return zone
-    }
-    
-    func stringNumberToPlay(zone: Int, oldZone: Int) -> Int {
-        return oldZone > zone ? oldZone : zone
-    }
-    
-    ///   Description: This method determines if we are in a new zone -
-    ///   and if we should then play note on appropriate string
-    /// - Parameter location:- the current location of the drag in global co-ordinates
-    func newDragLocation(_ location: CGPoint?) {
-        guard let location =  location else { return }
-        
-        let zone = getZone(loc: location)
-        if zone != formerZone {
-            Logger.viewCycle.notice("====> In New Zone: \(zone)")
-            
-            if formerZone >= 0 && appState?.isMuted == false {
-                if AVAudioSession.sharedInstance().outputVolume == 0.0 {
-                    appState?.showVolumeAlert = true
-                } else {
-                    let stringToPlay: Int = stringNumberToPlay(zone: zone, oldZone: formerZone)
-                    pickString(stringToPlay)
-                }
-            }
 
-            formerZone = zone
-        }
-    }
-    
-    /// Description: This method identifies the note to play on this string based on capo position and fret -
-    ///  and then plays that string if the string is not muted
-    /// - Parameter stringToPlay: The String to be played
-    func pickString(_ stringToPlay: Int) {
-        let openNotes = appState?.activeTuning?.openNoteIndices.components(separatedBy: "-")
-        
-        let fretPosition = appState?.currentFretIndexMap[6 - stringToPlay]
-        if fretPosition ?? 0 > kNoFret {
-            if let noteIndices = openNotes, let thisStringsOpenIndex = Int(noteIndices[6 - stringToPlay]) {
-                let index = (fretPosition ?? 0) + thisStringsOpenIndex + (appState?.capoPosition ?? 0 )
-                let noteToPlayName = noteNamesArray[index]
-                
-                let volume = appState?.volumeLevel ?? 0.5
-                Logger.viewCycle.debug("playing string: \(stringToPlay)")
-                playWaveFile(noteName:noteToPlayName,
-                             stringNumber: stringToPlay,
-                             volume: volume / 5.0)
-            }
-        }
-    }
-    
     func playWaveFile(noteName: String,
                       stringNumber: Int,
-                      volume: Double) {
+                      volume: Double) throws {
         let prefix = String(noteName.prefix(noteName.count - 4))  // trims ".wav" from end
         Logger.viewCycle.notice("-> playing String: \(stringNumber) note: \(noteName)")
         if let asset = NSDataAsset(name:prefix) {
@@ -159,10 +80,10 @@ class iJamAudioManager {
                 thisAudioPlayer.play()
             }
             catch InitializeErrors.AVAudioSessionError{
-                appState?.showAudioPlayerErrorAlert = true
+                throw InitializeErrors.AVAudioSessionError
             }
             catch {
-                appState?.showAudioPlayerErrorAlert = true
+                throw InitializeErrors.UnknownError
             }
         }
     }
