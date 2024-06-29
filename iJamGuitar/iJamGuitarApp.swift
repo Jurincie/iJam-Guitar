aca7805726d7758d918dccf12c89c4640c513ba7
RonJurincie <ronjurincie@gmail.com>
2024-06-29 18:06:45 -0400

    xx

--- iJamGuitar/iJamGuitarApp.swift
+++ iJamGuitar/iJamGuitarApp.swift
@@ -6,15 +6,27 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
 
 @main
 struct iJamGuitarApp: App {
     var shouldCreateUserDefaults: Bool = !UserDefaults.standard.bool(forKey: "DataExists")
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
   
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }.modelContainer(AppStateContainer.create(shouldCreateUserDefaults))
+        }
+        .modelContainer(AppStateContainer.create(shouldCreateUserDefaults))
     }
 }
 
